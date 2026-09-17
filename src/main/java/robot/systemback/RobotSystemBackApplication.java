6db57fd34a2b5e699b719c7a362546b96c6c5d58
lyd <16073921+lyd2952235159@user.noreply.gitee.com>
2026-09-17 14:30:15 +0800

    更新了一下项目介绍

---
+++ src/main/java/robot/systemback/RobotSystemBackApplication.java
@@ -0,0 +1,14 @@
+package robot.systemback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotSystemBackApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RobotSystemBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/robot/systemback/RobotSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package robot.systemback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotSystemBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
